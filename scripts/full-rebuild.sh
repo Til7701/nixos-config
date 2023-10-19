6d63d01d3bda9b5cb5c0077d1295a7e3588da3d6
Tilman Holube <tilman@holube.de>
2023-10-19 22:29:53 +0200

    dconf update -f

--- scripts/full-rebuild.sh
+++ scripts/full-rebuild.sh
@@ -1,5 +1,5 @@
 /home/tilman/nixos-config/scripts/rebuild.sh
-dconf reset /org/gnome/desktop
-dconf reset /org/gnome/shell
-dconf reset /org/gnome/mutter
-dconf reset /org/gnome/settings-daemon/plugins/media-keys/
+dconf reset -f /org/gnome/desktop
+dconf reset -f /org/gnome/shell
+dconf reset -f /org/gnome/mutter
+dconf reset -f /org/gnome/settings-daemon/plugins/media-keys/
