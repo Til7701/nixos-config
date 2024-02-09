23a3c8e00aa0e45d1d5602c863ba94db29ab898d
Tilman Holube <tilman@holube.de>
2024-02-09 11:20:31 +0100

    unstable kernel 6.6

--- T07LY9i/configuration.nix
+++ T07LY9i/configuration.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
 {
   networking.hostName = "T07LY9i";
 
@@ -7,13 +10,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
-  boot.kernelPackages = pkgs.linuxPackages_6_5;
+  boot.kernelPackages = unstable.linuxPackages_6_6;
 
   # Speaker Pin
   # https://wiki.archlinux.org/title/Lenovo_Yoga_9i_2022_(14AiPI7)
   # https://nixos.wiki/wiki/ALSA
   boot.extraModprobeConfig = ''
-  options snd-sof-intel-hda-common hda_model=alc287-yoga9-bass-spk-pin
+    options snd-sof-intel-hda-common hda_model=alc287-yoga9-bass-spk-pin
   '';
 
   # DisplayLink
@@ -23,7 +26,8 @@
   # '';
 
   # Suspend-then-hibernate everywhere https://www.worldofbs.com/nixos-framework/
-  services.logind = { # https://man7.org/linux/man-pages/man5/logind.conf.5.html
+  services.logind = {
+    # https://man7.org/linux/man-pages/man5/logind.conf.5.html
     lidSwitch = "suspend-then-hibernate";
     extraConfig = ''
       HandlePowerKey=suspend-then-hibernate
