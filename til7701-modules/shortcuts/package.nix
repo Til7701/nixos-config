501021f93244d6db65e6bc59f6d3656fe407790a
Tilman Holube <tilman@holube.de>
2024-03-08 21:31:50 +0100

    channel shortcut

--- til7701-modules/shortcuts/package.nix
+++ til7701-modules/shortcuts/package.nix
@@ -7,20 +7,33 @@ in
 
 stdenv.mkDerivation rec {
   pname = "shortcuts";
-  version = "0.0.5";
+  version = "0.0.6";
+
+  icons = ./icons;
 
   unpackPhase = "true";
 
   installPhase = ''
     mkdir -pv $out/share/applications/
-      cp -r "${localConfigDI}/share/applications" "$out/share/applications/"
+    cp -vr "${localConfigDI}/share/applications" "$out/share/"
+    cp -vr "${tilChannelDI}/share/applications" "$out/share/"
+
+    mkdir -v $out/share/pixmaps
+    ln -sv "$icons/the_game_icon.png" $out/share/pixmaps
   '';
 
   localConfigDI = makeDesktopItem {
-    name = "NixOS Config";
+    name = "NixOSConfig";
     exec = "code /home/tilman/nixos-config";
     icon = "noel";
     desktopName = "NixOS Config";
+  };
+
+  tilChannelDI = makeDesktopItem {
+    name = "NixOSTil7701Channel";
+    exec = "code /home/tilman/IdeaProjects/nix-channel";
+    icon = "the_game_icon";
+    desktopName = "NixOS Til7701 Channel";
   };
 
 }
