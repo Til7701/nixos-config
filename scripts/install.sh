8ec0a5dd088cce561b676046e9fcba17c51596ea
Tilman Holube <tilman@holube.de>
2024-05-25 08:49:51 +0200

    channel to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,6 @@
 
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+sudo nix-channel --add https://github.com/Til7701/nix-channel/archive/main.tar.gz til7701
+nix-channel --add https://github.com/schlunzis/nix-channel/archive/main.tar.gz schlunzis
 sudo nix-channel --update
