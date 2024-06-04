58ab6184d3bc5d9fdf532f460ed6a097a00ce9cc
Tilman Holube <tilman@holube.de>
2024-06-04 09:30:49 +0200

    vim syntax highlighting

--- til7701-modules/common.nix
+++ til7701-modules/common.nix
@@ -44,6 +44,8 @@ in
           " Start NERDTree. If a file is specified, move the cursor to its window.
           autocmd StdinReadPre * let s:std_in=1
           autocmd VimEnter * NERDTree | if argc() > 0 || exists("s:std_in") | wincmd p | endif
+
+          syntax on
         '';
       })
     ];
