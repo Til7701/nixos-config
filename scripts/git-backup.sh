b67b3d71f141a0f30a0128cf9bb06683abb8efbe
Tilman Holube <tilman@holube.de>
2024-06-10 10:42:39 +0200

    updating git backup script with blacklists

--- scripts/git-backup.sh
+++ scripts/git-backup.sh
@@ -19,7 +19,19 @@ backup_github() {
         fi
     }
 
+    is_blacklisted() {
+        local repo="$1"
+        local blacklist="$2"
+        for blacklisted_repo in $blacklist; do
+            if [ "$blacklisted_repo" = "$repo" ]; then
+                return 0
+            fi
+        done
+        return 1
+    }
+
     local username="$1"
+    local blacklist="$2"
     local repositories=$(fetch_repositories "$username")
 
     mkdir -pv ./$username
@@ -27,6 +39,10 @@ backup_github() {
 
     if [ -n "$repositories" ]; then
         for repo in $(echo "$repositories" | jq -r '.[] | .name'); do
+            if is_blacklisted "$repo" "$blacklist"; then
+                echo "Skipping blacklisted repository: $username/$repo"
+                continue
+            fi
             clone_repository "$username" "$repo" &
         done
         wait
@@ -41,8 +57,8 @@ repo_folder=repositories
 mkdir -pv $repo_folder
 cd $repo_folder
 
-backup_github Til7701
-backup_github schlunzis
+backup_github Til7701 ""
+backup_github schlunzis "ppa"
 
 cd ..
 
